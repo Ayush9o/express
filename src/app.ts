44d642b4b4708841d75b171863204503577ed45d
ayush9o <ayush2468.jnu@gmail.com>
2026-09-23 13:14:43 +0530

    working stage

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,12 @@ const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (_req, res) => {
+  res.json({
+    message: "server",
+    status: "running",
+  });
+});
 app.use("/api", healthRoutes);
 app.use("/api", echoRoutes);
 app.use((req: Request, res: Response) => {
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,7 @@
-const express = require("express");
-import type { Request, Response } from "express";
-const NODE_ENV = process.env.NODE_ENV;
-const PORT = process.env.PORT || 5000;
+import app from "./app";
 
-const app = express();
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
-});
+const PORT = Number(process.env.PORT) || 5000;
+const NODE_ENV = process.env.NODE_ENV || "development";
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT} (${NODE_ENV})`);
