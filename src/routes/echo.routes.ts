906b39d1ee52ffb1382c65416361f49347a3448e
ayush9o <ayush2468.jnu@gmail.com>
2026-09-23 10:18:43 +0530

    new update

--- src/routes/echo.routes.ts
+++ src/routes/echo.routes.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { echoController } from "../controller/echo.controller";
 const router = Router();
 
-router.get("/health", echoController);
+router.get("/echo", echoController);
 
 export default router;
